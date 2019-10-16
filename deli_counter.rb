6fa202daf3aef4b813473b6759658fd2172cc0fa
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-16 05:24:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,13 @@ def line(array)
    new_array = []
    counter = 1
    array.each do |name|
-     new_array.push
+     new_array.push"#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{new_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
 end  
 
 #def take_a_number(katz_deli, name)
