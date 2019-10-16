432525d24fd7446d982c2917a799dbe314540259
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-16 05:10:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 def line(array)
   if array.length == 0
   puts "The line is currently empty."
+  
 end
 end  
 
-def take_a_number(katz_deli, name)
-  katz_deli.push(name) 
-  puts "Welcome, #{name}, you are number #{line.length} in line."
-end  
+#def take_a_number(katz_deli, name)
+ # katz_deli.push(name) 
+  #puts "Welcome, #{name}, you are number #{line.length} in line."
+#end  
