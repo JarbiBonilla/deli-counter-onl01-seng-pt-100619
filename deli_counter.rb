938b0b71bfff0766b23cbdd1da858d86914fd230
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-16 05:26:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(array)
   end
 end  
 
-#def take_a_number(katz_deli, name)
- # katz_deli.push(name) 
-  #puts "Welcome, #{name}, you are number #{line.length} in line."
-#end  
+def take_a_number(katz_deli, name)
+  katz_deli.push(name) 
+  puts "Welcome, #{name}, you are number #{line.length} in line."
+end  
