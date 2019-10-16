dbb6ba0d3e3891030cd3df33a5f4b7985b3ed99f
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-16 05:23:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,6 @@
 def line(array)
-  if array.length == 0
-  puts "The line is currently empty."
- else array.length > 0
-   puts "The line is currently: #{array[1]}."
-end
+ if array.length >= 1
+   
 end  
 
 #def take_a_number(katz_deli, name)
