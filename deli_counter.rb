ad825324a9638d8566161933a78494edbb0c975f
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-16 04:59:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+  if array.length == 0
+  puts "The line is currently empty."
+end
+end  
+
+def take_a_number(line, new_person)
+  line.push(new_person) 
+  binding.pry
+  puts "Welcome, #{new_person}, you are number #{line.length} in line."
+end
+end  
