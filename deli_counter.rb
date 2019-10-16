a281dd220b5a022559a770b8319c9e5adba9c6cc
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-16 04:59:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def line(array)
   if array.length == 0
   puts "The line is currently empty."
@@ -8,7 +6,6 @@ end
 
 def take_a_number(line, new_person)
   line.push(new_person) 
-  binding.pry
   puts "Welcome, #{new_person}, you are number #{line.length} in line."
 end
 end  
